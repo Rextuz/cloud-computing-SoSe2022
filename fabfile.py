32a7f7401b036d49c8c4139e7048687ab4f790ed
Alex <alexanderwiegel17@gmail.com>
2022-09-25 17:00:36 +0200

    Add code for docker/database

--- fabfile.py
+++ fabfile.py
@@ -20,15 +20,13 @@ def delete_resource_group():
 
 
 def create_vm(name):
-    local(
-        f"az vm create"
-        f" --resource-group {RESOURCE_GROUP}"
-        f" --name {name}"
-        f" --size Standard_B1ls"
-        f" --image UbuntuLTS"
-        f" --public-ip-sku Standard"
-        f" --admin-username azureuser"
-    )
+    local(f"az vm create"
+          f" --resource-group {RESOURCE_GROUP}"
+          f" --name {name}"
+          f" --size Standard_B1ls"
+          f" --image UbuntuLTS"
+          f" --public-ip-sku Standard"
+          f" --admin-username azureuser")
 
 
 def install_nodejs():
@@ -39,6 +37,22 @@ def install_nodejs():
     run("node --version")
 
 
+# from https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+def install_docker():
+    run("sudo apt-get update")
+    run("sudo apt-get install ca-certificates curl gnupg lsb-release")
+    run("sudo mkdir -p /etc/apt/keyrings")
+    run("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg"
+        )
+    run("echo 'deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null"
+        )
+    run("sudo apt-get update")
+    run("sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin"
+        )
+    #run("sudo systemctl enable docker") # uncomment if necessary
+
+
 def copy_backend():
     run("mkdir -p /root/cloud_computing/backend")
     put("./app/backend", "/root/cloud_computing")
@@ -47,6 +61,11 @@ def copy_backend():
 def copy_frontend():
     run("mkdir -p /root/cloud_computing/frontend")
     put("./app/frontend", "/root/cloud_computing")
+
+
+def copy_database():
+    run("mkdir -p /root/cloud_computing/database")
+    put("./app/database/docker-compose.yml", "/root/cloud_computing/database")
 
 
 def install_backend():
@@ -66,6 +85,10 @@ def install_frontend():
         run("npm start")
 
 
+def start_docker():
+    run("docker compose up")
+
+
 def deploy_backend():
     azure_login()
     create_resource_group()
@@ -73,3 +96,9 @@ def deploy_backend():
     install_nodejs()
     copy_backend()
     install_backend()
+
+
+def deploy_database():
+    install_docker()
+    copy_database()
+    start_docker()
