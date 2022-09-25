f9d7f42f50e2823f90c8f4ea171f9667404ed876
Alex <alexanderwiegel17@gmail.com>
2022-09-25 13:16:27 +0200

    Write functions for setting up the frontend

--- fabfile.py
+++ fabfile.py
@@ -15,15 +15,26 @@ def copy_backend():
     put("./app/backend", "/root/cloud_computing")
 
 
+def copy_frontend():
+    run("mkdir -p /root/cloud_computing/frontend")
+    put("./app/frontend", "/root/cloud_computing")
+
+
 def install_backend():
     run("mkdir -p /storage/profile_uploads")
     run("mkdir -p /storage/course_uploads")
     with shell_env(
-        PROFILE_STORAGE="/storage/profile_uploads",
-        COURSE_STORAGE="/storage/course_uploads",
+            PROFILE_STORAGE="/storage/profile_uploads",
+            COURSE_STORAGE="/storage/course_uploads",
     ), cd("/root/cloud_computing/backend"):
         run("npm install")
         run("node index.js")
+
+
+def install_frontend():
+    with shell_env(CI="true"), cd("/root/cloud_computing/frontend"):
+        run("npm install")
+        run("npm start")
 
 
 def deploy_backend():
